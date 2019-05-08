f692ebddab8802a1fdcc17cb72a2fb37dbde285c
Poly Goveia <poly.goveia@gmail.com>
2019-05-07 20:19:51 -0700

    style card component

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -3,7 +3,7 @@ import './Card.css';
 
 const CardContent = () => {
   return (
-    <div>
+    <div className="card-content">
         <p> Get started with React </p>
         <p> React makes it painless to create interactive UIs.Design simple views for each state in your application. </p>
         <p className="link"> reactjs.org </p> 
