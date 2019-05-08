d70ce01d5cceca8eb907584283393406da504437
Poly Goveia <poly.goveia@gmail.com>
2019-05-07 19:06:32 -0700

    create HeaderContainer and import

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import './App.css';
 
+// import containers
+import HeaderContainer from './components/HeaderComponents/HeaderContainer';
+
 const App = () => {
   return (
     <div>
