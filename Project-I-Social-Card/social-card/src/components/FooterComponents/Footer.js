465198e5ef0399c7178fe4207424cb412a949c61
Poly Goveia <poly.goveia@gmail.com>
2019-05-08 17:14:07 -0700

    add click handler to like a post

--- Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -1,14 +1,43 @@
 import React, { Component } from 'react'
+import './Footer.css'
 
-export default class Footer extends Component {
+ class Footer extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            like: 0,
+            retweet: 0,
+            isLiked: false,
+            isRetweeted: false
+        }
+    }
+
+    handleLike = () => {
+      if (!this.state.isLiked) {
+        this.setState({
+          like: this.state.like +1,
+          isLiked: true
+        })
+      } else {
+         this.setState({
+           like: this.state.like -1,
+           isLiked: false
+         })
+      }
+    }
+    
+
   render() {
     return (
       <div className="footer">
-        <i class="far fa-comment fa-2x"></i>
-        <i class="fas fa-sync fa-2x"></i>
-        <i class="far fa-heart fa-2x"></i>
-        <i class="far fa-envelope fa-2x"></i>
+        <i className="far fa-comment fa-2x"></i>
+        <i className="fas fa-sync fa-2x"><span className="number">{this.state.retweet}</span></i>
+        <i onClick={this.handleLike} className="far fa-heart fa-2x"><span className="number">{this.state.like}</span></i>
+        <i className="far fa-envelope fa-2x"></i>
       </div>
     )
   }
 }
+
+export default Footer;
