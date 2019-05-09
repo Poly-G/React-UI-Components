833a6f453a46e821a8d7199827b6c5823cd100fa
Poly Goveia <poly.goveia@gmail.com>
2019-05-08 17:17:10 -0700

    add retweet click handler

--- Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -26,13 +26,27 @@ import './Footer.css'
          })
       }
     }
+
+   handleRetweet = () => {
+     if (!this.state.isRetweeted) {
+       this.setState({
+         retweet: this.state.retweet + 1,
+         isRetweeted: true
+       })
+     } else {
+       this.setState({
+         retweet: this.state.retweet - 1,
+         isRetweeted: false
+       })
+     }
+   }
     
 
   render() {
     return (
       <div className="footer">
         <i className="far fa-comment fa-2x"></i>
-        <i className="fas fa-sync fa-2x"><span className="number">{this.state.retweet}</span></i>
+        <i onClick={this.handleRetweet} className="fas fa-sync fa-2x"><span className="number">{this.state.retweet}</span></i>
         <i onClick={this.handleLike} className="far fa-heart fa-2x"><span className="number">{this.state.like}</span></i>
         <i className="far fa-envelope fa-2x"></i>
       </div>
